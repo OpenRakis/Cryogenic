05412a7175b722d50cd4b83fe959f1e7d3fa685e
Maximilien Noal <noal.maximilien@gmail.com>
2023-05-28 13:38:48 +0200

    fix: missed injected service (ILoggerPropertyBag)
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Cryogenic/DependencyInjectionExtensions.cs
+++ src/Cryogenic/DependencyInjectionExtensions.cs
@@ -1,13 +1,15 @@
 namespace Cryogenic;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 using Spice86.Logging;
 using Spice86.Shared.Interfaces;
 
 public static class LoggerServiceInjectionExtensions {
     public static IServiceCollection AddLogging(this IServiceCollection services) {
-        services.AddSingleton<ILoggerService, LoggerService>();
+        services.TryAddSingleton<ILoggerPropertyBag, LoggerPropertyBag>();
+        services.TryAddSingleton<ILoggerService, LoggerService>();
         return services;
     }
 }
