2dfb5aab76945ebf0e1ff73184b07ac2ccb34151
Maximilien Noal <noal.maximilien@gmail.com>
2023-10-07 11:42:06 +0200

    Redefined more of the MT-32 driver
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Cryogenic/Overrides/MT32DriverCode.cs
+++ src/Cryogenic/Overrides/MT32DriverCode.cs
@@ -9,11 +9,22 @@ public partial class Overrides {
 
     public void DefineMT32DriverCodeOverrides() {
         DefineFunction(0xF000, 0x100, DNMID_entry_00, true, nameof(DNMID_entry_00));
+        DefineFunction(0xF000, 0x100 + 0x17b, DNMID_entry_01, true, nameof(DNMID_entry_01));
         DefineFunction(0xF000, 0x1CB, DNMID_internal_function_00, true, nameof(DNMID_internal_function_00));
     }
 
+    private Action DNMID_entry_01(int arg) {
+        var result = Alu16.Xor(AX, AX);
+        if (result == 0) {
+            return NearJump(0x04);
+        }
+        Cpu.Out16(0x330, AX);
+        return NearJump(0x0);
+    }
+
     private Action DNMID_entry_00(int arg) {
-        return NearJump(0x17b);
+        return DNMID_entry_01(0);
+        //return NearJump(0x17b);
     }
 
     /// <summary>
