1f4d91118d79915d1e210fde898d395e10bdd725
Maximilien Noal <noal.maximilien@gmail.com>
2024-02-07 21:33:29 +0100

    Begun live savegame window
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Cryogenic/DuneCdOverrideSupplier.cs
+++ src/Cryogenic/DuneCdOverrideSupplier.cs
@@ -1,13 +1,18 @@
 namespace Cryogenic;
 
+using Avalonia.Threading;
+
 using Spice86.Core.CLI;
 using Spice86.Core.Emulator.Function;
-using Spice86.Core.Emulator.ReverseEngineer;
 using Spice86.Core.Emulator.VM;
 using Spice86.Shared.Emulator.Memory;
 using Spice86.Shared.Interfaces;
+using Spice86.Shared.UI;
 
 using System.Collections.Generic;
+
+using UI.ViewModels;
+using UI.Views;
 
 /// <summary>
 /// Example command line debug arguments: -e "C:\\Jeux\\ABWFR\\DUNE_CD\\C\\DNCDPRG.EXE" --UseCodeOverride true
@@ -21,6 +26,9 @@ public class DuneCdOverrideSupplier : IOverrideSupplier {
         Machine machine) {
         Dictionary<SegmentedAddress, FunctionInformation> res = new();
         CreateOverrides(loggerService, configuration, programStartSegment, machine, res);
+        Dispatcher.UIThread.Post(() => {
+            AdditionalWindow.Show(new LiveSaveGameWindow() { DataContext = new LiveSaveGameWindowViewModel(machine.Memory) });
+        });
         return res;
     }
 
---
+++ src/Cryogenic/UI/Views/LiveSaveGameWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Cryogenic.UI.Views;
+
+public partial class LiveSaveGameWindow : Window {
+    public LiveSaveGameWindow() {
+        InitializeComponent();
+    }
+}
