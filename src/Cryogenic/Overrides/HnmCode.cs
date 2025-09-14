e8848b62c862420a21d2b165651610f818f783fb
Kevin <kevinferrare@gmail.com>
2025-09-14 23:24:59 +0200

    [WIP] Adapt to latest spice86 version (#32)
    * Adapt to latest spice86 version
    
    * use v10 nuget
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    
    ---------
    
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>
    Co-authored-by: Maximilien Noal <noal.maximilien@gmail.com>

--- src/Cryogenic/Overrides/HnmCode.cs
+++ src/Cryogenic/Overrides/HnmCode.cs
@@ -24,17 +24,15 @@ public partial class Overrides {
         uint targetMemory = globalsOnDs.GetPtr1138_DC0C_Dword32_hnmFileReadBufferSegment().Linear;
         _loggerService.Debug("Read {@ReadLength} bytes from hnm file handle {@FileHandle} at offset {@Offset}", readLength, fileHandle, offset);
         dosFileManager.MoveFilePointerUsingHandle(0, fileHandle, offset);
-        DosFileOperationResult result = dosFileManager.ReadFile(fileHandle, readLength, targetMemory);
+        DosFileOperationResult result = dosFileManager.ReadFileOrDevice(fileHandle, readLength, targetMemory);
         uint? actualReadLength = result.Value;
         if (actualReadLength != readLength) {
             throw this.FailAsUntested("The original code loops here when read bytes from hnm are not as expected.");
         }
-        if (actualReadLength is not null) {
-            globalsOnDs.Set1138_DC08_DWord32_hnmFileRemain(globalsOnDs.Get1138_DC08_DWord32_hnmFileRemain() - actualReadLength.Value);
-            globalsOnDs.Set1138_DC04_DWord32_hnmFileOffset(offset + actualReadLength.Value);
-            globalsOnDs.Set1138_DC0C_Word16_hnmFileReadBufferSegment((ushort)(globalsOnDs.Get1138_DC0C_Word16_hnmFileReadBufferSegment() + actualReadLength.Value));
-            globalsOnDs.Set1138_DC1A_Word16((ushort)(globalsOnDs.Get1138_DC1A_Word16() + actualReadLength.Value));
-        }
+        globalsOnDs.Set1138_DC08_DWord32_hnmFileRemain(globalsOnDs.Get1138_DC08_DWord32_hnmFileRemain() - actualReadLength.Value);
+        globalsOnDs.Set1138_DC04_DWord32_hnmFileOffset(offset + actualReadLength.Value);
+        globalsOnDs.Set1138_DC0C_Word16_hnmFileReadBufferSegment((ushort)(globalsOnDs.Get1138_DC0C_Word16_hnmFileReadBufferSegment() + actualReadLength.Value));
+        globalsOnDs.Set1138_DC1A_Word16((ushort)(globalsOnDs.Get1138_DC1A_Word16() + actualReadLength.Value));
         return NearRet();
     }
 }
