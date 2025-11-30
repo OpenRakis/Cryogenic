1d584c0ce53357a7929b1ef70138229eba989d32
Maximilien Noal <noal.maximilien@gmail.com>
2025-11-30 18:56:44 +0100

    fix: window wasn't shown

--- src/Cryogenic/GameEngineWindow/GameEngineWindowManager.cs
+++ src/Cryogenic/GameEngineWindow/GameEngineWindowManager.cs
@@ -17,32 +17,29 @@ public static class GameEngineWindowManager {
     private static bool _isWindowOpen;
 
     public static void ShowWindow(IByteReaderWriter memory, SegmentRegisters segmentRegisters, IPauseHandler? pauseHandler = null) {
-        if (!Dispatcher.UIThread.CheckAccess()) {
-            Dispatcher.UIThread.Post(() => ShowWindow(memory, segmentRegisters, pauseHandler));
-            return;
-        }
+        Dispatcher.UIThread.Post(() => {
+            if (_window != null && _isWindowOpen) {
+                _window.Show();
+                _window.Activate();
+                return;
+            }
 
-        if (_window != null && _isWindowOpen) {
-            _window.Show();
-            _window.Activate();
-            return;
-        }
-
-        _viewModel?.Dispose();
-        _viewModel = new DuneGameStateViewModel(memory, segmentRegisters, pauseHandler);
-        _window = new DuneGameStateWindow {
-            DataContext = _viewModel
-        };
-
-        _window.Closed += (sender, args) => {
-            _isWindowOpen = false;
             _viewModel?.Dispose();
-            _viewModel = null;
-            _window = null;
-        };
+            _viewModel = new DuneGameStateViewModel(memory, segmentRegisters, pauseHandler);
+            _window = new DuneGameStateWindow {
+                DataContext = _viewModel
+            };
 
-        _isWindowOpen = true;
-        _window.Show();
+            _window.Closed += (sender, args) => {
+                _isWindowOpen = false;
+                _viewModel?.Dispose();
+                _viewModel = null;
+                _window = null;
+            };
+
+            _isWindowOpen = true;
+            _window.Show();
+        });
     }
 
     public static void CloseWindow() {
